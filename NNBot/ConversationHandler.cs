4d40f4292683a58f6bc57f4f4e1ce196c01cb21d
Michal Povinsky <antihutka@gmail.com>
2016-10-11 22:07:03 +0200

    Talkinfo implemented

--- NNBot/ConversationHandler.cs
+++ NNBot/ConversationHandler.cs
@@ -52,17 +52,19 @@ namespace NNBot
 			double talkProb = 0.01;
 			lock (lck)
 			{
-				selftalk *= 0.98;
-				othertalk *= 0.98;
+				double td = Convert.ToDouble(Bot.configuration["talkdecay"]);
+				selftalk *= td;
+				othertalk *= td;
 				double talkratio = (0 + selftalk) / (1 + othertalk);
 				//talkProb /= Math.Exp(5*talkratio);
 				talkProb /= Math.Pow(talkratio, 3) + 0.01;
 				if (othertalk > 750) talkProb /= Math.Exp((othertalk - 750)/100);
 				//if (timeTalked < 1) talkProb /= Math.Exp(6 - 6*timeTalked);
 				if (thinking) talkProb = 0;
-				Console.WriteLine("tHear=" + timeHeard.ToString("n4") + " tTalk=" + timeTalked.ToString("n4") +
-								  " oTalk=" + othertalk.ToString("n4") + " sTalk=" + selftalk.ToString("n4") +
-				                  " ratio=" + talkratio.ToString("n4") + " prob=" + talkProb.ToString("n4"));
+				if (Convert.ToInt32(Bot.configuration["talkinfo"]) > 1)
+					Console.WriteLine("tHear=" + timeHeard.ToString("n4") + " tTalk=" + timeTalked.ToString("n4") +
+					 			 	  " oTalk=" + othertalk.ToString("n4") + " sTalk=" + selftalk.ToString("n4") +
+				                      " ratio=" + talkratio.ToString("n4") + " prob=" + talkProb.ToString("n4"));
 			}
 
 			if (Bot.rand.NextDouble() < talkProb)
